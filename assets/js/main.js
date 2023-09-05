7797d515a2e79ba111eab627c7067a0ab2982c5b
Wilmarx John Cayabyab <50767502+zhackdtech@users.noreply.github.com>
2023-09-06 01:27:07 +0800

    added files via upload
    added sidenav for large screen and collapsed nav for small screens

--- assets/js/main.js
+++ assets/js/main.js
@@ -124,30 +124,42 @@ $(document).ready(function () {
     //particle js end
 
     //smooth scroll effect on anchors
-    $(document).ready(function () {
-        // Add smooth scrolling to all links
-        $("a").on('click', function (event) {
-
-            // Make sure this.hash has a value before overriding default behavior
-            if (this.hash !== "") {
-                // Prevent default anchor click behavior
-                event.preventDefault();
-
-                // Store hash
-                var hash = this.hash;
-
-                // Using jQuery's animate() method to add smooth page scroll
-                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
-                    scrollTop: $(hash).offset().top
-                }, 800, function () {
-
-                    // Add hash (#) to URL when done scrolling (default click behavior)
-                    window.location.hash = hash;
-                });
-            } // End if
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
         });
-    });
     //smooth scroll end
     //tilt effect on hover
     /* Store the element in el */
@@ -209,4 +221,19 @@ $(document).ready(function () {
         el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
     })
     //end tilt
+    //start hover on anchor
+    $('li.mynav').hover
+    (
+        function()
+        {
+            $(this).addClass('cyberpunk')
+            $(this).addClass('active')
+        },
+        function()
+        {
+            $(this).removeClass('cyberpunk')
+            $(this).removeClass('active')
+        }
+    )
+    //end hover
 })
